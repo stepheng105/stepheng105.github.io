416d9b28a2605aa353723af4de67166c2e93d228
sgcook <sgcook@calpoly.edu>
2024-10-25 09:03:20 -0700

    Adding new images and added link to code for summer2024

--- static/js/scripts.js
+++ static/js/scripts.js
@@ -2,7 +2,7 @@
 
 const content_dir = 'contents/'
 const config_file = 'config.yml'
-const section_names = ['home', 'publications', 'conferences', 'awards', 'experience', 'teaching', 'gallery']
+const section_names = ['home', 'publications', 'experience', 'conferences', 'awards', 'teaching', 'gallery']
 
 
 window.addEventListener('DOMContentLoaded', event => {
